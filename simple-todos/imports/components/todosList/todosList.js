fc47822a8a3478169ca104e8f74a9965ffc51837
meshnesh <tonny.nesh@gmail.com>
2016-05-18 14:59:04 +0300

    added checkbox and button

--- simple-todos/imports/components/todosList/todosList.js
+++ simple-todos/imports/components/todosList/todosList.js
@@ -10,11 +10,16 @@ class TodosListCtrl {
 
         this.helpers({
             tasks() {
-                return Tasks.find({});
+                // Show newest tasks at the top
+                return Tasks.find({}, {
+                    sort: {
+                        createdAt: -1
+                    }
+                });
             }
         })
     }
-    
+
     addTask(newTask) {
         // Insert a task into the collection
         Tasks.insert({
